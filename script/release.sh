8629a5a76613dc5675da6682bda328971b5b5fb8
Fatih Arslan <ftharsln@gmail.com>
2021-06-07 13:15:36 +0300

    Add release step

---
+++ script/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This fetches the data that was filled out in the previous block
+# step and outputs the values
+
+set -eu
+
+echo "+++ :construction:  Installing 'svu' tool"
+curl -sfL https://install.goreleaser.com/github.com/caarlos0/svu.sh | bash -s -- -b /usr/local/bin
+
+
+RELEASE_VERSION=$(/usr/local/bin/svu minor)
+
+
+echo "+++ :boom: Bumping to version $RELEASE_VERSION"
+
+echo "✅"
