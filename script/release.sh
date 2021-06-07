c54fc5dc6be4c1c4ac0166e3be5da9f23899f559
Fatih Arslan <ftharsln@gmail.com>
2021-06-07 13:58:36 +0300

    Push tag

--- script/release.sh
+++ script/release.sh
@@ -9,10 +9,11 @@ SVU_BIN="${WORKDIR}/svu"
 echo "+++ :construction:  Installing 'svu' tool"
 curl -sfL https://install.goreleaser.com/github.com/caarlos0/svu.sh | bash -s -- -b $WORKDIR
 
-
-RELEASE_VERSION=$($SVU_BIN minor)
-
+RELEASE_VERSION=$($SVU_BIN patch)
 
 echo "+++ :boom: Bumping to version $RELEASE_VERSION"
+
+git tag "$(svu next)"
+git push --tags
 
 echo "✅"
